dde7af464e86cfd88fc2b8c99f12d8869f88b56b
Francisco Mesa <francisco@leanmind.es>
2023-04-12 21:44:01 +0100

    :test_tube: test_aircraft_bounces_at_the_territory_northerneast_border

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -43,6 +43,15 @@ class TestAircraft(unittest.TestCase):
         assert_that(an_aircraft.current_position())\
             .is_equal_to(Position(longitude=4, latitude=1))
 
+    def test_aircraft_bounces_at_the_territory_northerneast_border(self):
+        territory = Territory(max_longitude=6, max_latitude=6)
+        an_aircraft = aircraft.create(Position(longitude=6, latitude=0), territory)
+
+        an_aircraft.move(Direction.NorthEast)
+
+        assert_that(an_aircraft.current_position())\
+            .is_equal_to(Position(longitude=4, latitude=1))
+
     def test_aircraft_bounces_at_the_territory_eastern_border(self):
         an_aircraft = aircraft.create(
             Position(longitude=5, latitude=4),
